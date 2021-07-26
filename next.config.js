e3b04b2c715259d17318d977b6e79d2a6c536b2b
Atharv Attri <atharv260107@gmail.com>
2021-07-25 18:40:50 -0700

    try to fix image loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   images: {
-    loader: 'imgix',
     domains: ['assets.vercel.com', 'dummyimage.com', "uploads-ssl.webflow.com", "tailwindui.com", "images.unsplash.com"],
     disableStaticImages: false,
   },
